d1d1251fa978147fead6129cbd682053a9ba35ed
Matt Werner <mrwerner392@gmail.com>
2019-04-04 18:51:36 +0000

    Automatically backed up by Learn

---
+++ index.js
@@ -0,0 +1,19 @@
+// add two numbers together
+function add(num1, num2) {
+  return num1 + num2;
+}
+
+// subtract one number from another
+function subtract(num1, num2) {
+  return num1 - num2;
+}
+
+// multiply two numbers together
+function multiply(num1, num2) {
+  return num1 * num2;
+}
+
+// divide one number by another
+function divide(num1, num2) {
+  return num1 / num2;
+}
